0ab41f89c31b5c261f2bf9fd14c2ca5967f9dee8
Valerian Coelho <valerian27012004@gmail.com>
2024-06-23 15:52:57 +0530

    Best First Search Bug Fixed and Edge Case Considered

--- Experiment 3/Best_First_Search.py
+++ Experiment 3/Best_First_Search.py
@@ -1,35 +1,45 @@
 def best_first_search(start, goal, graph):
     visited = []
-    open = [([start], 0)]  # this is a list of tuples, where each tuple is a path and its heuristic
+    open = [[start]]  # this is a list of paths
     while open:  # when open is empty, the search is over and there is no solution
         print(open)
-        path, heuristic = open.pop(0)
+        path = open.pop(0)
         node = path[-1]  # the current node is the last node in the path
         if node == goal:  # if the current node is the goal, return the path
             return path
         if node not in visited:
             visited.append(node)
-            for neighbour, neighbour_heuristic in graph[node]:
+            for neighbour in graph[node]:
                 new_path = path + [neighbour]
-                new_heuristic = heuristic + neighbour_heuristic
-                open.append((new_path, new_heuristic))
-            open.sort(key=lambda x: x[1])
+                open.append(new_path)
+            open.sort(key=lambda x: heuristic[x[-1]])
     return None
 
 
 start = "A"
-goal = "k"
+goal = "H"
 
 # format: {node: [(neighbour, heuristic)]}
 graph = {
-    "A": [("B", 12), ("C", 4)],
-    "B": [("D", 8), ("E", 7)],
-    "C": [("F", 6), ("G", 2)],
+    "A": ["B", "C"],
+    "B": ["D", "E"],
+    "C": ["F", "G"],
     "D": [],
-    "E": [("H", 0)],
-    "F": [("H", 0)],
-    "G": [("H", 0)],
+    "E": ["H"],
+    "F": ["H"],
+    "G": ["H"],
     "H": []
+}
+heuristic = {
+    "A": 10,
+    "B": 12,
+    "C": 4,
+    "D": 8,
+    "E": 7,
+    "F": 6,
+    "G": 2,
+    "H": 0,
+
 }
 
 print(best_first_search(start, goal, graph))
