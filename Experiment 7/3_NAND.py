759322445b77b21a73b5763ba4842eb9cb26f22a
Valerian Coelho <valerian27012004@gmail.com>
2024-06-23 23:16:16 +0530

    Function names fixed

--- Experiment 7/3_NAND.py
+++ Experiment 7/3_NAND.py
@@ -7,16 +7,16 @@ def perceptron(x, w, b):
     r = np.dot(x, w) + b
     return activation(r)
 
-def AND(x):
+def NAND(x):
     w = np.array([1, 1, 1])
     b = 0.5
     return perceptron(x, w, b)
 
-print(AND(np.array([0, 0, 0])))
-print(AND(np.array([0, 0, 1])))
-print(AND(np.array([0, 1, 0])))
-print(AND(np.array([0, 1, 1])))
-print(AND(np.array([1, 0, 0])))
-print(AND(np.array([1, 0, 1])))
-print(AND(np.array([1, 1, 0])))
-print(AND(np.array([1, 1, 1])))
+print(NAND(np.array([0, 0, 0])))
+print(NAND(np.array([0, 0, 1])))
+print(NAND(np.array([0, 1, 0])))
+print(NAND(np.array([0, 1, 1])))
+print(NAND(np.array([1, 0, 0])))
+print(NAND(np.array([1, 0, 1])))
+print(NAND(np.array([1, 1, 0])))
+print(NAND(np.array([1, 1, 1])))
--- Experiment 7/3_NOR.py
+++ Experiment 7/3_NOR.py
@@ -7,16 +7,16 @@ def perceptron(x, w, b):
     r = np.dot(x, w) + b
     return activation(r)
 
-def OR(x):
+def NOR(x):
     w = np.array([1, 1, 1])
     b = 0.5
     return perceptron(x, w, b)
 
-print(OR(np.array([0, 0, 0])))
-print(OR(np.array([0, 0, 1])))
-print(OR(np.array([0, 1, 0])))
-print(OR(np.array([0, 1, 1])))
-print(OR(np.array([1, 0, 0])))
-print(OR(np.array([1, 0, 1])))
-print(OR(np.array([1, 1, 0])))
-print(OR(np.array([1, 1, 1])))
+print(NOR(np.array([0, 0, 0])))
+print(NOR(np.array([0, 0, 1])))
+print(NOR(np.array([0, 1, 0])))
+print(NOR(np.array([0, 1, 1])))
+print(NOR(np.array([1, 0, 0])))
+print(NOR(np.array([1, 0, 1])))
+print(NOR(np.array([1, 1, 0])))
+print(NOR(np.array([1, 1, 1])))
